5a0f441efc771096edd4c3e314c855d8c1081f7b
ArnavNikam <75774457+ArnavNikam@users.noreply.github.com>
2021-06-14 19:26:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,26 +3,7 @@ block_Width = 30;
 block_Height = 30;
 document.getElementById("width-span").innerHTML = block_Width;
 document.getElementById("height-span").innerHTML = block_Height;
-playerX = 120;
-playerY = 136;
-storePlayer = "";
 storeBlock = "";
-
-function PlayerUpdate() {
-    fabric.Image.fromURL("zack-1.png", function(IMG)
-    {
-        storePlayer = IMG;
-        storePlayer.scaleToWidth(150);
-        storePlayer.scaleToHeight(140);
-        storePlayer.set(
-            {
-                top:playerY,
-                left:playerX
-            }
-        );
-        canvas.add(storePlayer);
-    });
-}
 
 function BlockUpdate(GetIMG) {
     fabric.Image.fromURL(GetIMG, function(IMG)
@@ -38,4 +19,41 @@ function BlockUpdate(GetIMG) {
         );
         canvas.add(storeBlock);
     });
+}
+
+window.addEventListener("keydown", myKeyDown);
+
+function myKeyDown(e) {
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+
+    if(keyPressed == 70) {
+        new_image("ironman_face.png")
+        console.log("f");
+    }
+
+    if(keyPressed == 66) {
+        new_image("ironman_body.png")
+        console.log("b");
+    }
+
+    if(keyPressed == 76) {
+        new_image("ironman_legs.png")
+        console.log("l");
+    }
+
+    if(keyPressed == 82) {
+        new_image("ironman_right_hand.png")
+        console.log("r");
+    }
+
+    if(keyPressed == 72) {
+        new_image("ironman_left_hand.png")
+        console.log("h");
+    }
+
+    if(keyPressed == 38) {
+        up()
+        console.log("up");
+    }
 }
