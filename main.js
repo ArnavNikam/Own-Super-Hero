55c75f160469cd2b72fb0f2dc1cf7a42059daf6d
ArnavNikam <75774457+ArnavNikam@users.noreply.github.com>
2021-06-15 15:30:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,29 @@ block_Width = 30;
 block_Height = 30;
 document.getElementById("width-span").innerHTML = block_Width;
 document.getElementById("height-span").innerHTML = block_Height;
+playerX = 120;
+playerY = 136;
+storePlayer = "";
 storeBlock = "";
 
-function BlockUpdate(GetIMG) {
+function PlayerUpdate() {
+    fabric.Image.fromURL("zack-1.png", function(IMG)
+    {
+        storePlayer = IMG;
+        storePlayer.scaleToWidth(150);
+        storePlayer.scaleToHeight(140);
+        storePlayer.set(
+            {
+                top:playerY,
+                left:playerX
+            }
+        );
+        canvas.add(storePlayer);
+    });
+}
+
+
+function new_image(GetIMG) {
     fabric.Image.fromURL(GetIMG, function(IMG)
     {
         storeBlock = IMG;
@@ -54,6 +74,54 @@ function myKeyDown(e) {
 
     if(keyPressed == 38) {
         up()
-        console.log("up");
+    }
+
+    if(keyPressed == 39) {
+        right()
+    }
+
+    if(keyPressed == 40) {
+        down()
+    }
+
+    if(keyPressed == 37) {
+        left()
+    }
+
+}
+
+function up() {
+    if(playerY >= 0) {
+        console.log("top is pressed");
+        playerY = playerY - block_Height;
+        canvas.remove(storePlayer);
+        PlayerUpdate();
+    }
+}
+
+function down() {
+    if(playerY <= 450) {
+        console.log("down is pressed");
+        playerY = playerY + block_Height;
+        canvas.remove(storePlayer);
+        PlayerUpdate();
+    }
+}
+
+function left() {
+    if(playerX >= 0) {
+        console.log("left is pressed");
+        playerX = playerX - block_Width;
+        canvas.remove(storePlayer);
+        PlayerUpdate();
+    }
+}
+
+function right() {
+    if(playerX <= 650) {
+        console.log("right is pressed");
+        playerX = playerX + block_Width;
+        canvas.remove(storePlayer);
+        PlayerUpdate();
     }
 }
